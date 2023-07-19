195e4385be0f79f029567389e270d6f090c6bf67
Aikz <rauldiogoms@gmail.com>
2023-07-19 11:54:24 +0100

    Adicionar arquivos de projeto.

---
+++ StackOverflowPost/Post.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflowPost
+{
+    internal class Post
+    {
+        //Create a program where we should be able to
+        //Upvote , Downvote a post , See current vote value.
+        //in the main method, create a post, up-vote and down-vote it a feew times
+        //and then display the current vote value
+
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DateTime { get; set; }
+        public int UpVote { get; set; }
+        public int DownVote { get; set; }
+
+
+        private static List<Post> AllPosts = new List<Post>();
+
+        public Post(string PostTitle , string PostDescription , DateTime PostDate)
+        {
+            Title = PostTitle; Description = PostDescription; DateTime = PostDate;
+            Id = AllPosts.Count;
+            AllPosts.Add(this);
+        }
+
+        public static List<Post> GetAllPosts()
+        {
+            return AllPosts;
+        }
+
+        public static Post VotePost(int Id)
+        {
+            Console.WriteLine("Vote with key Arrows (Up - Upvote , Down - Downvote)");
+            var keyInfo = Console.ReadKey();
+
+            switch (keyInfo.Key.ToString())
+            {
+                case "DownArrow":
+
+                    int DownvoteNumber = AllPosts[Id].DownVote;
+
+                    DownvoteNumber = DownvoteNumber + 1;
+
+                    AllPosts[Id].DownVote = DownvoteNumber;
+
+                    return AllPosts[Id];
+
+                break;
+
+                case "UpArrow":
+
+                    int UpvoteNumber = AllPosts[Id].UpVote;
+
+                    UpvoteNumber = UpvoteNumber + 1;
+
+                    AllPosts[Id].UpVote = UpvoteNumber;
+
+                    return AllPosts[Id];
+
+
+                break;
+
+                default:
+                    Console.Clear();
+                    Console.WriteLine("Invalid Input!");
+                    VotePost(Id);
+                    break;
+            }
+
+
+            return AllPosts[Id];
+        }
+
+
+
+        
+
+        
+
+
+
+    }
+}
---
+++ StackOverflowPost/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StackOverflowPost
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+
+            Console.WriteLine("A - Ver / Votar posts");
+            Console.WriteLine("B - Criar post");
+            userInput = Console.ReadLine().ToLower();
+
+            switch (userInput)
+            {
+                case "a":
+
+                    List<Post> posts = Post.GetAllPosts();
+
+                    if(posts.Count == 0)
+                    {
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("No Posts");
+                        Console.ResetColor();
+                        Main(args);
+                        break;
+                    }
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("a - Return");
+                        Console.WriteLine("Insert ID Post number to vote");
+                        Console.ResetColor();
+                        Console.WriteLine("");
+
+                    foreach (Post post in posts)
+                    {
+                        Console.WriteLine("Id: " + post.Id);
+                        Console.WriteLine("Title: " + post.Title);
+                        Console.WriteLine("Description: " + post.Description);
+                        Console.WriteLine("Date: " + post.DateTime);
+                        Console.WriteLine("Up Votes: " + post.UpVote);
+                        Console.WriteLine("Down Votes: " + post.DownVote);
+                        Console.WriteLine("----------------------");
+                    }
+
+                    string inputUser;
+
+                    inputUser = Console.ReadLine().ToLower();
+
+                    if (inputUser == "a")
+                    {
+                        Console.Clear();
+                        Main(args);
+                    }
+                    else
+                    {
+                        try
+                        {
+                            if(int.Parse(inputUser) <= posts.Count)
+                            {
+                                Post.VotePost(int.Parse(inputUser));
+                                Console.Clear();
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                Console.WriteLine("Voted!");
+                                Console.ResetColor();
+
+                                Main(args);
+                            }
+                            else {Console.Clear(); Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("Invalid ID"); Console.ResetColor(); Main(args); }
+                            
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.Clear();
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Invalid ID");
+                            Console.ResetColor();
+                            Main(args);
+                        }
+                    }
+                    break;
+
+                case "b":
+
+                    Console.Clear();
+                    Console.WriteLine("");
+                    Console.WriteLine("Insert Post Title: ");
+                    string postTitle = Console.ReadLine();
+                    Console.WriteLine("Insert Post Description: ");
+                    string postDescription = Console.ReadLine();
+
+                    //Create a new post
+                    Post newPost = new Post(postTitle, postDescription, DateTime.Now);
+
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Post Created");
+                    Console.ResetColor();
+                    Main(args);
+
+                    break;
+            }
+
+        }
+    }
+}
